16807983e98e9372ee38a79953d97745a6b8f48e
nilsnils191 <nilsnils191@gmail.com>
2020-05-01 14:06:48 +0530

    Spring Boot MVC with basic index.jsp page , http://localhost:8090/helloJsp

---
+++ springbootmvc/src/main/java/com/mvc/boot/springbootmvc/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.mvc.boot.springbootmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+	
+	@GetMapping("/helloJsp")
+	public String init() {
+		return "index";
+	}
+}
